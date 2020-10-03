6de06bb8955b8d903e45367cae3473c41ed81420
Praney Behl <praneybehl@gmail.com>
2020-10-04 08:47:41 +1000

    chore: setup application constants

---
+++ src/constants/console-message.ts
@@ -0,0 +1,14 @@
+export enum ConsoleMessage {
+	TITLE = "🤖 ToyBot Simulator",
+	BANNER = "Toy Robot Simulator V1- interactive command-line application for coding challenge",
+	START_BANNER = "      Starting ToyBot Simulator V1!      ",
+	STOP_BANNER = "      ToyBot Simulator Complete      ",
+	TXT_PARSING_ERROR = "Parsing text file data, please check file data",
+	INVALID_POSITION = "Invalid position coordinates value",
+	POSITION_INPUT = "Toy Robot needs to be placed on the table, enter it's position in coordinates:",
+	ZOMBIE_DIRECTION_MSG = `
+Robot Needs to be facing in a direction when placed on the table,
+Select Robot's directions: NORTH, SOUTH, WEST, EAST
+`,
+	OUTPUT_SCORE = "Output: ",
+}
