24d8968ad273eb5fb631ef54f9c531f2fe1a322d
Praney Behl <praneybehl@gmail.com>
2020-10-04 18:56:51 +1100

    chore: update simulator to pass table bounds to toybot move method

--- src/simulator.ts
+++ src/simulator.ts
@@ -1,19 +1,17 @@
-import Toybot, { Directions } from "./toybot";
-import Table, { IPosition } from "./table";
-import { showOutput } from "./utils";
+import Toybot, { IToyBot } from "./toybot";
+import Table from "./table";
+import { showError, showOutput, simpleLog } from "./utils";
+import { ConsoleMessage } from "./constants/console-message";
 
 export default class Command {
-	private toybot: Toybot | undefined = undefined;
+	private toybot?: Toybot;
 	private table: Table;
 
 	constructor(table: Table) {
 		this.table = table;
 	}
 
-	public run(
-		command: string,
-		options?: { position: IPosition; direction: Directions }
-	): void {
+	public run(command: string, options?: IToyBot): boolean {
 		if (this.toybot) {
 			switch (command) {
 				case "LEFT":
@@ -23,23 +21,33 @@ export default class Command {
 					this.toybot && this.toybot.turnRight();
 					break;
 				case "MOVE":
-					this.toybot && this.toybot.move();
+					this.toybot &&
+						this.toybot.move(
+							this.table.width - 1,
+							this.table.height - 1
+						);
 					break;
 				case "REPORT":
 					this.toybot && showOutput(this.toybot.report());
 					break;
+				case "EXIT":
+					return false;
 				default:
+					showError(ConsoleMessage.INVALID_COMMAND);
+					simpleLog(ConsoleMessage.HELP);
+					break;
 			}
 		}
 
 		if (command === "PLACE" && options) {
 			if (this.table.isInBounds(options.position)) {
-				if (!this.toybot) {
-					this.toybot = new Toybot({ ...options, isPlaced: true });
-				} else {
+				if (this.toybot) {
 					this.toybot.place(options);
+				} else {
+					this.toybot = new Toybot({ ...options, isPlaced: true });
 				}
 			}
 		}
+		return true;
 	}
 }
