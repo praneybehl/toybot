e305a217466705fff8e060eef74cb84e2105e4f3
Praney Behl <praneybehl@gmail.com>
2020-10-04 17:37:42 +1100

    chore: update constants

--- src/constants/console-message.ts
+++ src/constants/console-message.ts
@@ -1,14 +1,21 @@
 export enum ConsoleMessage {
 	TITLE = "🤖 ToyBot Simulator",
 	BANNER = "Toy Robot Simulator V1- interactive command-line application for coding challenge",
+	ERROR = "ERROR: ",
 	START_BANNER = "      Starting ToyBot Simulator V1!      ",
 	STOP_BANNER = "      ToyBot Simulator Complete      ",
-	TXT_PARSING_ERROR = "Parsing text file data, please check file data",
-	INVALID_POSITION = "Invalid position coordinates value",
-	POSITION_INPUT = "Toy Robot needs to be placed on the table, enter it's position in coordinates:",
-	ZOMBIE_DIRECTION_MSG = `
-Robot Needs to be facing in a direction when placed on the table,
-Select Robot's directions: NORTH, SOUTH, WEST, EAST
-`,
+	INVALID_POSITION = "Invalid position coordinates value, please check and retry",
+	INVALID_DIRECTION = "Invalid facing direction value, please check and retry with one of: NORTH | SOUTH | EAST | WEST",
+	INVALID_COMMAND = "Invalid command, please check and type a valid command.",
+	PROMPT = "Enter command",
+	HELP = `
+Commands List:
+PLACE 1,2,EAST 	---> Places the robot on x:1, y: 2 facing EAST to the Table
+MOVE 			---> Move the robot 1 step in the direction it's facing
+RIGHT 			---> Turn the robot right
+LEFT 			---> Turn the robot left
+REPORT 			---> Report robot's position and facing direction on the table
+EXIT 			---> Exit Application
+	`,
 	OUTPUT_SCORE = "Output: ",
 }
