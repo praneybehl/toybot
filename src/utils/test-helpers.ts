ee828ea4db0a456d42ffa9f2b22b41a49f8c189b
Praney Behl <praneybehl@gmail.com>
2020-10-07 21:42:38 +1100

    test: add test helps for mocking console statements

---
+++ src/utils/test-helpers.ts
@@ -0,0 +1,23 @@
+export function spyConsole() {
+	const spy: any = {
+		console: {},
+	};
+
+	beforeAll(() => {
+		spy.console.error = jest
+			.spyOn(console, "error")
+			.mockImplementation(() => {});
+		spy.console.info = jest
+			.spyOn(console, "info")
+			.mockImplementation(() => {});
+		spy.console.log = jest
+			.spyOn(console, "log")
+			.mockImplementation(() => {});
+	});
+
+	afterAll(() => {
+		spy.console.mockRestore();
+	});
+
+	return spy;
+}
